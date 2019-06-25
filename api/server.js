6c509f1de417afbf3fe11b3ed1d5c186096b484a
Kory Newton <korybnewton@gmail.com>
2019-06-25 14:41:26 -0700

    test data

--- api/server.js
+++ api/server.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const authRouter = require('../auth/auth-router.js');
-const condRouter = require('../conditions/condition-router.js')
-const server = express()
+const condRouter = require('../conditions/condition-router.js');
+const tempRouter = require('./temporary');
+const server = express();
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
 
-server.get('/',(req,res) => {
-    res.status(200).json({"api":"up"})
-})
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up' });
+});
 
-server.use('/auth',authRouter);
-server.use('/conditions',condRouter);
+server.use('/auth', authRouter);
+server.use('/conditions', condRouter);
+server.use('/temp', tempRouter);
 
-module.exports = server
+module.exports = server;
